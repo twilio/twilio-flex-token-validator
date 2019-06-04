cf5dd64c05a82c32ac1ab0e33e1d6a79aeef727a
Kousha Talebian <kousha.talebian@gmail.com>
2019-06-04 14:55:56 -0700

    add a linter

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,155 +5,155 @@ import * as nock from 'nock';
 // const https = require('https');
 
 describe('index.ts', () => {
-	beforeAll(() => {
-		nock.disableNetConnect()
-	});
+    beforeAll(() => {
+        nock.disableNetConnect()
+    });
 
-	afterAll(() => {
-		nock.enableNetConnect();
-	});
+    afterAll(() => {
+        nock.enableNetConnect();
+    });
 
-	beforeEach(() => {
-		jest.clearAllMocks();
-	});
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
 
-	const mockHttps = () => {
-		return nock('https://iam.twilio.com')
-			.post(() => true);
-	};
+    const mockHttps = () => {
+        return nock('https://iam.twilio.com')
+            .post(() => true);
+    };
 
-	describe('validator', () => {
-		afterEach(() => {
-			nock.cleanAll();
-		});
+    describe('validator', () => {
+        afterEach(() => {
+            nock.cleanAll();
+        });
 
-		it('should fail if no token is provided', async (done) => {
-			try {
-				await validator('', '', '');
-			} catch (err) {
-				expect(err).toEqual('Token was not provided');
-				done();
-			}
-		});
+        it('should fail if no token is provided', async (done) => {
+            try {
+                await validator('', '', '');
+            } catch (err) {
+                expect(err).toEqual('Token was not provided');
+                done();
+            }
+        });
 
-		it('should fail if no accountSid is provided', async (done) => {
-			try {
-				await validator('token-123', '', '');
-			} catch (err) {
-				expect(err).toContain('AccountSid or AuthToken was not provided');
-				done();
-			}
-		});
+        it('should fail if no accountSid is provided', async (done) => {
+            try {
+                await validator('token-123', '', '');
+            } catch (err) {
+                expect(err).toContain('AccountSid or AuthToken was not provided');
+                done();
+            }
+        });
 
-		it('should fail if no authToken is provided', async (done) => {
-			try {
-				await validator('token-123', 'AC123', '');
-			} catch (err) {
-				expect(err).toContain('AccountSid or AuthToken was not provided');
-				done();
-			}
-		});
+        it('should fail if no authToken is provided', async (done) => {
+            try {
+                await validator('token-123', 'AC123', '');
+            } catch (err) {
+                expect(err).toContain('AccountSid or AuthToken was not provided');
+                done();
+            }
+        });
 
-		it('should handle request failure', async (done) => {
-			const scope = mockHttps()
-				.replyWithError('this failed');
+        it('should handle request failure', async (done) => {
+            const scope = mockHttps()
+                .replyWithError('this failed');
 
-			try {
-				await validator('token-123', 'AC123', 'authToken');
-			} catch (err) {
-				expect(scope.isDone()).toBeTruthy();
-				expect(err).toEqual('this failed');
-				done();
-			}
-		});
+            try {
+                await validator('token-123', 'AC123', 'authToken');
+            } catch (err) {
+                expect(scope.isDone()).toBeTruthy();
+                expect(err).toEqual('this failed');
+                done();
+            }
+        });
 
-		it('should fail to validate if response is not json', async (done) => {
-			const scope = mockHttps()
-				.reply(200, 'not-json-string');
+        it('should fail to validate if response is not json', async (done) => {
+            const scope = mockHttps()
+                .reply(200, 'not-json-string');
 
-			try {
-				await validator('token-123', 'AC123', 'authToken');
-			} catch (err) {
-				expect(scope.isDone()).toBeTruthy();
-				expect(err).toContain('Unexpected token');
-				expect(err).toContain('in JSON at position');
-				done();
-			}
-		});
+            try {
+                await validator('token-123', 'AC123', 'authToken');
+            } catch (err) {
+                expect(scope.isDone()).toBeTruthy();
+                expect(err).toContain('Unexpected token');
+                expect(err).toContain('in JSON at position');
+                done();
+            }
+        });
 
-		it('should fail to validate if response valid is false', async (done) => {
-			const scope = mockHttps()
-				.reply(200, '{"valid":false, "message":"not valid"}');
+        it('should fail to validate if response valid is false', async (done) => {
+            const scope = mockHttps()
+                .reply(200, '{"valid":false, "message":"not valid"}');
 
-			try {
-				await validator('token-123', 'AC123', 'authToken');
-			} catch (err) {
-				expect(scope.isDone()).toBeTruthy();
-				expect(err).toEqual('not valid');
-				done();
-			}
-		});
+            try {
+                await validator('token-123', 'AC123', 'authToken');
+            } catch (err) {
+                expect(scope.isDone()).toBeTruthy();
+                expect(err).toEqual('not valid');
+                done();
+            }
+        });
 
-		it('should validate', async () => {
-			const scope = nock('https://iam.twilio.com')
-				.post('/v1/Accounts/AC123/Tokens/validate', {token: 'token-123'})
-				.reply(200, '{"valid":true, "other": "parameter"}');
+        it('should validate', async () => {
+            const scope = nock('https://iam.twilio.com')
+                .post('/v1/Accounts/AC123/Tokens/validate', {token: 'token-123'})
+                .reply(200, '{"valid":true, "other": "parameter"}');
 
-			const response = await validator('token-123', 'AC123', 'authToken');
-			expect(response).toEqual({valid: true, other: 'parameter'});
+            const response = await validator('token-123', 'AC123', 'authToken');
+            expect(response).toEqual({valid: true, other: 'parameter'});
 
-			expect(scope.isDone()).toBeTruthy();
-		});
-	});
+            expect(scope.isDone()).toBeTruthy();
+        });
+    });
 
-	describe('runtimeValidator', () => {
-		const context: Context = {
-			ACCOUNT_SID: 'AC123',
-			AUTH_TOKEN: 'AUTH123',
-		};
+    describe('runtimeValidator', () => {
+        const context: Context = {
+            ACCOUNT_SID: 'AC123',
+            AUTH_TOKEN: 'AUTH123',
+        };
 
-		const event: Event = {
-			Token: 'Token123'
-		};
+        const event: Event = {
+            Token: 'Token123'
+        };
 
-		afterEach(() => {
-			nock.cleanAll();
-		});
+        afterEach(() => {
+            nock.cleanAll();
+        });
 
-		it('should fail to validate', async () => {
-			const fn = jest.fn();
-			const cb = jest.fn();
-			const setStatusCode = jest.fn();
-			const setBody = jest.fn();
-			(global as any).Twilio.Response = jest.fn().mockImplementation(() => ({
-				setStatusCode,
-				setBody
-			}));
+        it('should fail to validate', async () => {
+            const fn = jest.fn();
+            const cb = jest.fn();
+            const setStatusCode = jest.fn();
+            const setBody = jest.fn();
+            (global as any).Twilio.Response = jest.fn().mockImplementation(() => ({
+                setStatusCode,
+                setBody
+            }));
 
-			await runtimeValidator(fn)(context, event, cb);
+            await runtimeValidator(fn)(context, event, cb);
 
-			expect(fn).not.toHaveBeenCalled();
-			expect(cb).toHaveBeenCalledTimes(1);
-			expect(setStatusCode).toHaveBeenCalledTimes(1);
-			expect(setBody).toHaveBeenCalledTimes(1);
+            expect(fn).not.toHaveBeenCalled();
+            expect(cb).toHaveBeenCalledTimes(1);
+            expect(setStatusCode).toHaveBeenCalledTimes(1);
+            expect(setBody).toHaveBeenCalledTimes(1);
 
-			expect(setStatusCode).toHaveBeenCalledWith(403);
-		});
+            expect(setStatusCode).toHaveBeenCalledWith(403);
+        });
 
-		it('should validate', async () => {
-			const scope = nock('https://iam.twilio.com')
-				.post('/v1/Accounts/AC123/Tokens/validate', {token: event.Token})
-				.reply(200, '{"valid":true, "other": "parameter"}');
+        it('should validate', async () => {
+            const scope = nock('https://iam.twilio.com')
+                .post('/v1/Accounts/AC123/Tokens/validate', {token: event.Token})
+                .reply(200, '{"valid":true, "other": "parameter"}');
 
-			const fn = jest.fn();
-			const cb = jest.fn();
+            const fn = jest.fn();
+            const cb = jest.fn();
 
-			await runtimeValidator(fn)(context, event, cb);
+            await runtimeValidator(fn)(context, event, cb);
 
-			expect(fn).toHaveBeenCalledTimes(1);
-			expect(cb).not.toHaveBeenCalled();
+            expect(fn).toHaveBeenCalledTimes(1);
+            expect(cb).not.toHaveBeenCalled();
 
-			expect(scope.isDone()).toBeTruthy();
-		});
-	});
+            expect(scope.isDone()).toBeTruthy();
+        });
+    });
 });
