193ba04459b590fd7138892789a7fac7fd1e0549
Kousha Talebian <kousha.talebian@gmail.com>
2020-07-22 10:34:12 -0700

    Bump packages

--- src/index.ts
+++ src/index.ts
@@ -14,44 +14,6 @@ export type Callback = (error: any, response: Twilio.Response) => void;
 export type HandlerFn = (context: Context, event: Event, callback: Callback) => void;
 
 /**
- * A validator to be used with Twilio Function. It uses the {@link validator} to validate the token
- *
- * @param handlerFn    the Twilio Runtime Handler Function
- */
-export const functionValidator = (handlerFn: HandlerFn): HandlerFn => {
-  return (context, event, callback) => {
-    const failedResponse = (message: string) => {
-      const response = new Twilio.Response();
-      response.appendHeader('Access-Control-Allow-Origin', '*');
-      response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST GET');
-      response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
-      response.appendHeader('Content-Type', 'plain/text');
-      response.setStatusCode(403);
-      response.setBody(message);
-
-      callback(null, response);
-    };
-
-    const accountSid = context.ACCOUNT_SID;
-    const authToken = context.AUTH_TOKEN;
-    const token = event.Token;
-
-    if (!accountSid || !authToken) {
-      return failedResponse(
-        'Unauthorized: AccountSid or AuthToken was not provided. For more information, please visit https://twilio.com/console/runtime/functions/configure',
-      );
-    }
-
-    return validator(token, accountSid, authToken)
-      .then((result) => {
-        event.TokenResult = result;
-        return handlerFn(context, event, callback);
-      })
-      .catch(failedResponse);
-  };
-};
-
-/**
  * Validates that the Token is valid
  *
  * @param token        the token to validate
@@ -106,4 +68,42 @@ export const validator = async (token: string, accountSid: string, authToken: st
     req.write(requestData);
     req.end();
   });
+};
+
+/**
+ * A validator to be used with Twilio Function. It uses the {@link validator} to validate the token
+ *
+ * @param handlerFn    the Twilio Runtime Handler Function
+ */
+export const functionValidator = (handlerFn: HandlerFn): HandlerFn => {
+  return (context, event, callback) => {
+    const failedResponse = (message: string) => {
+      const response = new Twilio.Response();
+      response.appendHeader('Access-Control-Allow-Origin', '*');
+      response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST GET');
+      response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+      response.appendHeader('Content-Type', 'plain/text');
+      response.setStatusCode(403);
+      response.setBody(message);
+
+      callback(null, response);
+    };
+
+    const accountSid = context.ACCOUNT_SID;
+    const authToken = context.AUTH_TOKEN;
+    const token = event.Token;
+
+    if (!accountSid || !authToken) {
+      return failedResponse(
+        'Unauthorized: AccountSid or AuthToken was not provided. For more information, please visit https://twilio.com/console/runtime/functions/configure',
+      );
+    }
+
+    return validator(token, accountSid, authToken)
+      .then((result) => {
+        event.TokenResult = result;
+        return handlerFn(context, event, callback);
+      })
+      .catch(failedResponse);
+  };
 };
